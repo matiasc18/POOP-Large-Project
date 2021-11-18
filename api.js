bedaf9f03295ed95bacf71ae71fd5a3eea4c600c
evgeduardo96 <90790092+evgeduardo96@users.noreply.github.com>
2021-11-18 14:10:39 -0800

    edit api

--- api.js
+++ api.js
@@ -3,9 +3,9 @@ const User = require("./models/user.js");
 //load card model
 const StrengthExercise = require("./models/strengthExercise.js"); 
 const CardioExercise = require("./models/cardioExercise.js");
-const bcrypt = require("bcrypt");
 
 var token = require('./createJWT.js');
+const { findByIdAndRemove, findByIdAndDelete, findByIdAndUpdate } = require("./models/user.js");
 
 exports.setApp = function ( app, client )
 {
@@ -23,7 +23,7 @@ exports.setApp = function ( app, client )
 
       const { login, password } = req.body;
 
-      const results = await User.find({ Login: login});
+      const results = await User.find({ Login: login, Password: password });
 
       var id = -1;
       var fn = '';
@@ -32,8 +32,7 @@ exports.setApp = function ( app, client )
 
       var ret;
 
-      // Only runs if there exists a user with correct login and correct hashed password
-      if( results.length > 0 && await bcrypt.compare(password, results[0].Password))
+      if( results.length > 0 )
       {
         id = results[0]._id;
         fn = results[0].FirstName;
@@ -106,7 +105,7 @@ exports.setApp = function ( app, client )
     //////////////////////////////////////////////////////
     //                     CARDS                        //
     //////////////////////////////////////////////////////
-    
+
     app.post('/api/addexercise', async (req, res, next) =>
     {
       // incoming: userId, exerciseName, exerciseType, lowerRepRange,
@@ -237,4 +236,70 @@ exports.setApp = function ( app, client )
 
       res.status(200).json(ret);
     });
+
+    app.put('/api/edit', async (req, res, next) => 
+    {
+      // incoming: _id , exerciseName, exerciseType, lowerRepRange,
+      //           upperRepRange, strengthWeight, cardioTime, jwtToken
+      // outgoing: error
+      
+      const {_id, exerciseName, exerciseType, lowerRepRange, upperRepRange, strengthWeight, cardioTime, jwtToken } = req.body;
+
+      try
+      {
+        if( token.isExpired(jwtToken))
+        {
+          var r = {error:'The JWT is no longer valid', jwtToken: ''};
+          res.status(200).json(r);
+          return;
+        }
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
+
+        var error = '';
+        //Edit exerciseName, lowerRepRange, upperRepRange, strengthWeight, cardioTime to the exercise Type (strenght or cardio)
+        try
+        {
+          if (exerciseType.toLowerCase() === "strength")
+            {
+              // Edit  streght exercise
+              await StrengthExercise.findById(_id,(error, StrenghtEdit) =>
+              { 
+                StrenghtEdit.ExerciseName =  String(exerciseName), StrenghtEdit.LowerRepRange = Number(lowerRepRange), StrenghtEdit.UpperRepRange = Number(upperRepRange), StrenghtEdit.StrengthWeight = Number(strengthWeight)
+                StrenghtEdit.save();
+              });
+            }
+            else if (exerciseType.toLowerCase() === "cardio")
+            {
+              // Edit  cardio exercise
+              await CardioExercise.findById(_id,(error, CardioEdit) => 
+              { 
+                CardioEdit.ExerciseName = String(exerciseName), CardioEdit.CardioTime = Number(cardioTime)     
+                CardioEdit.save();
+              });  
+            }  
+        }
+        catch (e) 
+        {
+            error = e.toString();
+        }
+
+      var refreshedToken = null;
+      try
+      {
+        refreshedToken = token.refresh(jwtToken);
+      }
+      catch(e)
+      {
+        console.log(e.message);
+      }
+
+      var ret = { error: error, jwtToken: refreshedToken };
+
+      res.status(200).json(ret);
+    });
+
 }
